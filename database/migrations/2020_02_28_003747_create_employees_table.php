2bb825c86d3772b63939f05c2e9e0a22e878e292
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-27 21:34:54 -0500

    add/update project files

---
+++ database/migrations/2020_02_28_003747_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->tinyInteger('age');
+            $table->string('favorite color');
+            $table->binary('profile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
