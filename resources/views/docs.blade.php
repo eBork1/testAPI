b561eba5ca4c295bd0b4ae6a7dceeb6998772e4f
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-27 21:33:08 -0500

    create docs view

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Docs</title>
+</head>
+<body>
+    <p class="display-2">Docs</p>
+    <p>
+        example url for the test api: <strong>/api/employees/{id}</strong>
+    </p>
+</body>
+</html>
