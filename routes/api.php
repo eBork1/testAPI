fb92ca8f08fb70c8733b5b5bbd3cfd3b7f86d5ae
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-27 21:30:01 -0500

    add employee route & add login/logout routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
+// Auth + Login
+Route::post('/login','AuthenticationController@login')->name('login');
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/logout','AuthenticationController@logout')->name('logout');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Main user routes for getting the sample data 
+Route::get('/employees', 'EmployeeController@index');
+
+Route::get('/employees/{id}', 'EmployeeController@getByID');
+
