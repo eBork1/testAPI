86aedea98f35631e1983d2b58df1b25c9efd24ab
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-11 01:20:19 -0400

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Develofill</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title">
-                    ETB-Web Testing API
+                    DeveloFill Testing API
                 </div>
                 <p>A RESTFUL API for populating your website with sample text, images, and other content while in development.</p>
 
