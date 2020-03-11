bf633f020d80c05c89624df07eee48886daad4fe
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-10 20:18:30 -0400

    create middleware for checking api token validity

---
+++ app/Http/Middleware/ApiToken.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+class ApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user_token = $request->header('api_token');
+        $exists = DB::table('oauth_access_tokens')->where('id', $user_token)->first();
+        if (!$exists) {
+            return response()->json('Unauthorized - 401', 401);
+        }
+        return $next($request);
+    }
+}
+
