473e3becd751396d605e291149868c3612e237d4
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-11 01:19:40 -0400

    update documentation

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -9,10 +9,46 @@
                 <p class="display-2">Documentation</p>
             </div>
         </div>
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>
+                    Examples of how to call the develofill api
+                </h3>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12">
-                <p>Here is an example url for our test api:
+                <p class="pt-3">Here is an example url for our test api:
                     <strong>http://develofill.com/api/<code>content</code>/<code>{id}</code></strong>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="pt-3">
+                    Using <a href="https://github.com/axios/axios" target="_blank" rel="noreferrer noopener">Axios</a> <strong>(preferred)</strong>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5 mx-auto text-left bg-light">
+                axios({<br>
+                method: 'get',<br>
+                url: 'http://127.0.0.1:8000/api/employees',<br>
+                responseType: 'json'<br>
+                headers: {
+                    api_token: <code>your api token</code>
+                }<br>
+                })
+                .then(function(response) {<br>
+                console.log(response)<br>
+                });<br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="pt-3">
+                    Using <strong>Ajax</strong>
                 </p>
             </div>
         </div>
