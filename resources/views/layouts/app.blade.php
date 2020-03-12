5ce202741373232ded03918d099d672d72243531
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-11 20:37:07 -0400

    update layout view, add logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,24 +53,15 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item">
+                            <a  class="nav-link" href="http://127.0.0.1:8000/profile">
+                                {{ Auth::user()->name }}
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a  class="nav-link" href="{{ url('/logout') }}">
+                                Logout
+                            </a>
                         </li>
                     @endguest
                 </ul>
