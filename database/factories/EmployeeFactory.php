3add4267e741dcb71bad76b24c019f841afa0616
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-11 20:39:28 -0400

    create employee factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(employee::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'age' => $faker->randomNumber($nbDigits = 2, $strict = false),
+        'favorite_color' => $faker->safeColorName,
+        'job_title' => $faker->jobTitle,
     ];
 });
