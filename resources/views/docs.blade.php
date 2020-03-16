e0235a4f8f37c5df1f338f2d9701c2db2910bbbb
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-16 17:45:53 -0400

    update documentation

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -27,11 +27,11 @@
             <div class="col-12">
                 <p class="pt-3">
                     Using <a
-                            href="https://github.com/axios/axios"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            >Axios</a>
-                            <strong>(preferred)</strong>
+                        href="https://github.com/axios/axios"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >Axios</a>
+                    <strong>(preferred)</strong>
                 </p>
             </div>
         </div>
@@ -42,7 +42,7 @@
                 url: 'http://127.0.0.1:8000/api/employees',<br>
                 responseType: 'json'<br>
                 headers: {
-                    api_token: <code>your api token</code>
+                api_token: <code>your api token</code>
                 }<br>
                 })
                 .then(function(response) {<br>
@@ -74,10 +74,36 @@
             </div>
         </div>
         <div class="row mt-5">
+            <div class="col-8 mx-auto">
+                <h3>
+                    Authentication
+                </h3>
+                <br>
+                <p>
+                    To make a call to the API, you must send your API token as a header in your request. To get
+                    a token, you can <a href="{{ url('/register') }}">sign up</a> for an account and find your token in your profile. This token is a
+                    personal access token meaning it does not refresh or follow a full authorization flow, so keep
+                    it safe. Avoid exposing it through client sided code and do not share it.
+                </p>
+            </div>
+        </div>
+        <div class="row mt-5">
             <div class="col-12">
                 <h3>
                     Get Employees Data
                 </h3>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-12">
+                <p>
+                    Get <strong>all</strong> employees
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5 mx-auto bg-light">
+                http://127.0.0.1:8000/api/employees
             </div>
         </div>
     </div>
