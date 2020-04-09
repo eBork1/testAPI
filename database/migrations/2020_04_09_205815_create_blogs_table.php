3f5ad091d7779e6d0a9d09cbea3dcf7db0ebef5b
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-09 17:40:23 -0400

    add blog post migration

---
+++ database/migrations/2020_04_09_205815_create_blogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->longText('body')->nullable();
+            $table->string('title')->nullable();
+            $table->string('author')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
